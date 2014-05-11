b8adf7f4fa9d0077faa8c50277c0e3b231555e85
ichafrisca <frisca.ledy@yahoo.co.id>
2014-05-11 21:54:34 +0700

    menambahkan js pada header admin

--- application/views/headeradmin.php
+++ application/views/headeradmin.php
@@ -26,5 +26,12 @@
 				</div>
 			</div>
 		</center>
+		<!-- javascript foundation -->
+	<script type="text/javascript" src="<?php echo base_url(); ?>assets/foundation/js/vendor/modernizr.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>assets/foundation/js/vendor/jquery.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>assets/foundation/js/foundation.min.js"></script>  
+	<script type="text/javascript">
+		$(document).foundation();
+	</script>
 	</body>
 </html>
