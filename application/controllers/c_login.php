fc158ec41c90d4d25f1c1215d32a2aa92be755cc
ichafrisca <frisca.ledy@yahoo.co.id>
2014-10-26 00:00:45 +0700

    Pengecekan session untuk menghindari double login di browser yang sama

--- application/controllers/c_login.php
+++ application/controllers/c_login.php
@@ -3,7 +3,46 @@
 class C_login extends CI_Controller {
 
 	public function index(){
-		$this->load->view('login');
+		$sesi = $this->session->userdata('logged_id');
+
+		// jika session kosong maka kembali ke halaman login
+		if (empty($sesi)) {
+			$this->load->view('login');	
+
+		// jika session tidak kosong maka cek isi session
+		} else {
+
+			// jika isi session Admin, maka redirect ke halaman admin
+			if ($sesi == 'Admin') {
+				$this->load->model('m_logadmin');
+				$this->load->model('m_dtpegawai');
+
+				$p = 0;
+				$jumlah_per_page = 5;
+				
+				//pagination
+				$this->load->library('pagination');
+				$this->load->model('m_dtpegawai');
+
+				// pengaturan pagination
+				$config['base_url'] = site_url().'/c_dtpegawai/page/'; //url pagination
+				$config['total_rows'] = $this->m_dtpegawai->total_pegawai(); //total select
+				$config['per_page'] = $jumlah_per_page; //total select perhalaman
+
+				// masang pengaturan ke pagination
+				$this->pagination->initialize($config);
+
+				$data["pagination"] = $this->pagination->create_links(); // buat link pagination di view
+				$data["query"] = $this->m_dtpegawai->get_pegawai_page($p, $jumlah_per_page);
+				$data['query'] = $this->m_dtpegawai->get_pegawai_page();
+
+				$this->load->view('headeradmin', $data);
+			} else {
+				
+				// jika isi session bukan Admin, maka redirect ke user
+				$this->load->view('homeuser');
+			}
+		}
 	}
 
 	public function login($p = 0 ){
@@ -20,6 +59,10 @@ class C_login extends CI_Controller {
 			$status = $this->db->get('PEGAWAI')->result();
 			
 			foreach ($status as $key) {
+
+				// set session dengan status
+				$this->session->set_userdata('logged_id', $key->STATUS);
+
 				if ($key->STATUS == 'Admin') {
 					$this->session->set_userdata($data1);  //ngeset session isinya 'USERNAME','PASSWORD', 'STAT_PEG'
 					$this->load->model('m_logadmin');
@@ -43,12 +86,7 @@ class C_login extends CI_Controller {
 					$data['query'] = $this->m_dtpegawai->get_pegawai_page();
 
 					$this->load->view('headeradmin', $data);
-				} else if ($key->STATUS == 'Tutor'){
-					$this->session->set_userdata("pengguna", $key->IDPEG);
-					$this->session->set_userdata("nama_pengguna", $key->NAMA);
-					// $this->session->set_userdata("username_pengguna", $key->USERNAME);
-					$this->load->view('homeuser');
-				} else if ($key->STATUS == 'Office') {
+				} else if ($key->STATUS == 'Tutor' || $key->STATUS == 'Office'){
 					$this->session->set_userdata("pengguna", $key->IDPEG);
 					$this->session->set_userdata("nama_pengguna", $key->NAMA);
 					// $this->session->set_userdata("username_pengguna", $key->USERNAME);
